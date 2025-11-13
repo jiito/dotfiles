8a60805d3506d55115d72e38f62677d43cf9b56f
jiito <benjamin.allanrahill@gmail.com>
2025-11-13 11:00:16 -0800

    add save map

--- .config/nvim/lua/jiito/remap.lua
+++ .config/nvim/lua/jiito/remap.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>s", vim.cmd.w)
 
 -- Copy to clipboard
 vim.keymap.set('v', '<leader>y', '"+y',  { noremap = true, silent = true, desc = 'Copy to clipboard' })
@@ -13,4 +14,3 @@ vim.keymap.set('n', '<leader>p', '"+p', { noremap = true, silent = true, desc = 
 vim.keymap.set('n', '<leader>P', '"+P', { noremap = true, silent = true, desc = 'Paste before from clipboard' })
 vim.keymap.set('v', '<leader>p', '"+p', { noremap = true, silent = true, desc = 'Paste from clipboard' })
 vim.keymap.set('v', '<leader>P', '"+P', { noremap = true, silent = true, desc = 'Paste before from clipboard' })
-
